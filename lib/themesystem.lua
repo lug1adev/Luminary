c3962af6b1d099658674699ef03a64825d4b6dfd
natsu.ps1 <105063755+lug1adev@users.noreply.github.com>
2023-06-16 18:23:56 +0700

    Add files via upload

--- lib/themesystem.lua
+++ lib/themesystem.lua
@@ -13,6 +13,7 @@
 
 -- // Additional Library
 local fileSys = loadstring(game:HttpGet("https://raw.githubusercontent.com/lug1adev/Luminary/main/lib/filesystem.lua"))("lug1a#0001")
+local utils = loadstring(game:HttpGet("https://raw.githubusercontent.com/lug1adev/Luminary/main/lib/utils.lua"))("lug1a#0001")
 
 -- // Module Library
 local lib = {
@@ -23,7 +24,11 @@ local lib = {
         frame = {},
         textlabel = {},
         imagelabel = {}
-    }
+    },
+
+    EasingSpeed = 0.25,
+    EasingStyle = Enum.EasingStyle.Exponential,
+    EasingDirection = Enum.EasingDirection.InOut
 }
 
 function endsWith(str, suffix)
@@ -74,7 +79,23 @@ end
 function updateTheme()
     local currentTheme = lib.currentTheme
     if currentTheme then
-        
+        for i, v in pairs(lib.object.frame) do
+            utils.tween(i, {lib.EasingSpeed, lib.EasingStyle, lib.EasingDirection}, {
+                BackgroundColor3 = v
+            })
+        end
+
+        for i, v in pairs(lib.object.textlabel) do
+            utils.tween(i, {lib.EasingSpeed, lib.EasingStyle, lib.EasingDirection}, {
+                TextColor3 = v
+            })
+        end
+
+        for i, v in pairs(lib.object.imagelabel) do
+            utils.tween(i, {lib.EasingSpeed, lib.EasingStyle, lib.EasingDirection}, {
+                ImageColor3 = v
+            })
+        end
     end
 end
 
@@ -94,3 +115,5 @@ function registryObject(object : GuiObject, color)
         lib.object.imagelabel[object] = color
     end
 end
+
+return lib
